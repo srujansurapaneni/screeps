786296ce9293a7c870652ca425101e86dc31313d
srujansurapaneni <srujansurapaneni@gmail.com>
2020-05-09 11:39:38 -0400

    added defense code

---
+++ defense/main.js
@@ -0,0 +1,36 @@
+var roleHarvester = require("role.harvester");
+var roleUpgrader = require("role.upgrader");
+var roleBuilder = require("role.builder");
+
+module.exports.loop = function () {
+  var tower = Game.getObjectById("6efb5dfab64432f44ec59c01");
+  if (tower) {
+    var closestDamagedStructure = tower.pos.findClosestByRange(
+      FIND_STRUCTURES,
+      {
+        filter: (structure) => structure.hits < structure.hitsMax,
+      }
+    );
+    if (closestDamagedStructure) {
+      tower.repair(closestDamagedStructure);
+    }
+
+    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if (closestHostile) {
+      tower.attack(closestHostile);
+    }
+  }
+
+  for (var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    if (creep.memory.role == "harvester") {
+      roleHarvester.run(creep);
+    }
+    if (creep.memory.role == "upgrader") {
+      roleUpgrader.run(creep);
+    }
+    if (creep.memory.role == "builder") {
+      roleBuilder.run(creep);
+    }
+  }
+};
